ea4e27fd891a774c80ba2d8792a13992518d7e25
Ke Ma <kema2012@gmail.com>
2017-07-25 14:18:24 +0100

    finish

---
+++ ExpandableUITableCell/ExpandableUITableCell/Scenes/SceneOne/CustomCell/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  ExpandableUITableCell
+//
+//  Created by Ke Ma on 21/07/2017.
+//  Copyright © 2017 Ke Ma. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+  @IBOutlet weak var collapisbleViewLabel: UILabel!
+
+
+}
